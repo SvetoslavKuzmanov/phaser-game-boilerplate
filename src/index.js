6c7a14d3d41af775d53a423310d5dd357fe90b23
Svetoslav Kuzmanov <svetoslav@spotme.com>
2016-09-26 15:59:46 +0300

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import 'pixi'
+import 'p2'
+import { Game } from 'phaser'
+
+window.game = new Game()
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var path = require('path')
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// Phaser webpack config
+var phaserModule = path.join(__dirname, '/node_modules/phaser/')
+var phaser = path.join(phaserModule, 'build/custom/phaser-split.js')
+var pixi = path.join(phaserModule, 'build/custom/pixi.js')
+var p2 = path.join(phaserModule, 'build/custom/p2.js')
+
+module.exports = {
+    entry: {
+        app: [
+            'babel-polyfill',
+            path.resolve(__dirname, 'src/index.js')
+        ]
+    },
+    output: {
+        path: 'build',
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [{
+            test: /pixi\.js/,
+            loader: 'expose?PIXI'
+        }, {
+            test: /phaser-split\.js$/,
+            loader: 'expose?Phaser'
+        }, {
+            test: /p2\.js/,
+            loader: 'expose?p2'
+        }, {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015']
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js'],
+        alias: {
+            'phaser': phaser,
+            'pixi': pixi,
+            'p2': p2
+        }
+    },
+    plugins: [new HtmlWebpackPlugin()]
+}
